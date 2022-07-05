fa57cf6d7c3cc964c90e4440aeb704b2370022c7
Upal <71349329+Upal113@users.noreply.github.com>
2022-07-05 18:59:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,29 +24,31 @@ if ticker:
         column1.dataframe(historical_data)
         column2.dataframe(devidents)
         for devident in devidents.reset_index().values.tolist():
+            years.append(devident[0].year)
             msg = "On " + str(devident[0]) + " devident was " +str(devident[1])
             st.write(msg + '\n')
             ten_days_earlier = devident[0] - datetime.timedelta(days=14)
             before_date = historical_data[historical_data['Date'] >= ten_days_earlier]['Date'].min()
             price_on_ten_days_before = historical_data[historical_data['Date'] == before_date].values.tolist()[0][3]
-            st.write("10 days earlier price Date: " + str(before_date) + " Price: " + str(price_on_ten_days_before)  + '\n')
             target_price = price_on_ten_days_before + devident[1]
             st.write("Calculated target price  " + str(target_price))
             filter = (historical_data['Date'] >= ten_days_earlier) & (historical_data['High'] >= target_price)
             date_to_reach_terget = historical_data[filter]['Date'].min()
-            st.write("Date it reached the target  " + str(date_to_reach_terget)  + '\n')
             day_difference = date_to_reach_terget - before_date
+            days_taken.append(day_difference.days)
             if day_difference.days == 0:
                 st.write("Days required to reach target: " + "Same date"  + '\n')
             else:  
                 st.write("Days required to reach target: " + str(day_difference.days)  + '\n')
-        fig = go.Figure(data=[go.Candlestick(x=historical_data['Date'],
-                open=historical_data['Open'],
-                high=historical_data['High'],
-                low=historical_data['Low'],
-                close=historical_data['Close'])])  
-        st.title('Candelstick chart for the stock : ' + str(ticker))        
-        st.plotly_chart(fig)      
+        days_calculation_df = pd.DataFrame(columns = ['Year', 'Days Taken'])
+        days_calculation_df['Year'] = years
+        days_calculation_df['Days Taken'] = days_taken
+        st.dataframe(days_calculation_df)
+        for day_cal in days_calculation_df.groupby(['Year']).mean().reset_index().values.tolist():
+          st.write("Average days taken to reach target price in the year : " + str(day_cal[0]))
+          st.write(day_cal[1])
+        st.write('Average days taken to reach target price over the 10 years : ')
+        st.write(np.mean(days_calculation_df.groupby(['Year']).mean().reset_index()['Days Taken'].tolist()))     
     except:
         st.write("You have entered the wrong symbol")          
   
