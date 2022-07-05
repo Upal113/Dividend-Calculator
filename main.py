7c3ebf41803d1d050939e2c9d13d6f05420da1d3
Upal <71349329+Upal113@users.noreply.github.com>
2022-07-06 02:27:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,25 +24,21 @@ if tickers:
         years = []
         days_taken = []
         historical_data = data.get_data_yahoo(ticker, start, end).reset_index()
-        devidents = yf.Ticker(ticker).dividends.loc[start:end]
+        devidents = yf.Ticker(ticker).dividends.loc[start:end].reset_index()
         devidents = devidents[devidents['Date'].dt.month == datetime.datetime.today().month]
-        column1, column2, column3 = st.columns(3)
-        column1.dataframe(historical_data)
-        column2.dataframe(devidents)
-        for devident in devidents.reset_index().values.tolist():
-            years.append(devident[0].year)
-            msg = "On " + str(devident[0]) + " devident was " +str(devident[1])
-            ten_days_earlier = devident[0] - datetime.timedelta(days=14)
-            before_date = historical_data[historical_data['Date'] >= ten_days_earlier]['Date'].min()
-            price_on_ten_days_before = historical_data[historical_data['Date'] == before_date].values.tolist()[0][3]
-            target_price = price_on_ten_days_before + devident[1]
-            filter = (historical_data['Date'] >= ten_days_earlier) & (historical_data['High'] >= target_price)
-            date_to_reach_terget = historical_data[filter]['Date'].min()
-            day_difference = date_to_reach_terget - before_date
-            if day_difference.days == 0:
-              days_taken.append(1)
-            else:
-              days_taken.append(day_difference.days)
+        for devident in devidents.values.tolist():
+          years.append(devident[0].year)
+          ten_days_earlier = devident[0] - datetime.timedelta(days=14)
+          before_date = historical_data[historical_data['Date'] >= ten_days_earlier]['Date'].min()
+          price_on_ten_days_before = historical_data[historical_data['Date'] == before_date].values.tolist()[0][3]
+          target_price = price_on_ten_days_before + devident[1]
+          filter = (historical_data['Date'] >= ten_days_earlier) & (historical_data['High'] >= target_price)
+          date_to_reach_terget = historical_data[filter]['Date'].min()
+          day_difference = date_to_reach_terget - before_date
+          if day_difference.days == 0:
+            days_taken.append(1)
+          else:
+            days_taken.append(day_difference.days)
         days_calculation_df = pd.DataFrame(columns = ['Year', 'Days Taken'])
         days_calculation_df['Year'] = years
         days_calculation_df['Days Taken'] = days_taken
